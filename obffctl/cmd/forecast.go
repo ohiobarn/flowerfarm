91f4dde2e3cc7579e59b20a05adcd622403729ba
tgilkerson <tgilkerson@miletwo.us>
2021-07-07 23:57:02 -0400

    forcast command progress

--- obffctl/cmd/forecast.go
+++ obffctl/cmd/forecast.go
@@ -24,12 +24,67 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// SKU #,Image,Crop,Variety,Grower,SKU,This Week,Next Week,Future,Stems per Bunch,Availability Months,Category,This week | Next week | Future,Open in Shop,Shop Description,Color,Tier,Preorder Table,Preorder
+//
+// Forecast
+//
+type Forecast []ForecastDoc
 
-type Forecast []ForecastRec
+type ForecastDoc struct {
+	SKU                string `json:"SKU #"`
+	Image              string `json:"Image"`
+	Crop               string `json:"Crop"`
+	Variety            string `json:"Variety"`
+	Grower             string `json:"Grower"`
+	ThisWeek           string `json:"This Week"`
+	NextWeek           string `json:"Next Week"`
+	Future             string `json:"Future"`
+	StemsPerBunch      string `json:"Stems per Bunch"`
+	AvailabilityMonths string `json:"Availability Months"`
+	Category           string `json:"Category"`
+	ThisNextFuture     string `json:"This week | Next week | Future"`
+	OpenInShop         string `json:"Open in Shop"`
+	Color              string `json:"Color"`
+	Tier               string `json:"Tier"`
+}
 
-type ForecastRec struct {
-	SKU string `json:"SKU #"`
+//
+// Products
+//
+type Products []ProductsDoc
+
+type ProductsDoc struct {
+	ProductID       string `json:"Product ID [Non Editable]"`
+	VariantID       string `json:"Variant ID [Non Editable]""`
+	ProductType     string `json:"Product Type [Non Editable]"`
+	ProductPage     string `json:"Product Page"`
+	ProductURL      string `json:"Product URL"`
+	Title           string `json:"Title"`
+	Description     string `json:"Description"`
+	SKU             string `json:"SKU"`
+	OptionName1     string `json:"Option Name 1"`
+	OptionValue1    string `json:"Option Value 1"`
+	OptionName2     string `json:"Option Name 2"`
+	OptionValue2    string `json:"Option Value 2"`
+	OptionName3     string `json:"Option Name 3"`
+	OptionValue3    string `json:"Option Value 3"`
+	OptionName4     string `json:"Option Name 4"`
+	OptionValue4    string `json:"Option Value 4"`
+	OptionName5     string `json:"Option Name 5"`
+	OptionValue5    string `json:"Option Value 5"`
+	OptionName6     string `json:"Option Name 6"`
+	OptionValue6    string `json:"Option Value 6"`
+	Price           string `json:"Price"`
+	SalePrice       string `json:"Sale Price"`
+	OnSale          string `json:"On Sale"`
+	Stock           string `json:"Stock"`
+	Categories      string `json:"Categories"`
+	Tags            string `json:"Tags"`
+	Weight          string `json:"Weight"`
+	Length          string `json:"Length"`
+	Width           string `json:"Width"`
+	Height          string `json:"Height"`
+	Visible         string `json:"Visible"`
+	HostedImageURLs string `json:"Hosted Image URLs"`
 }
 
 // forecastCmd represents the forecast command
@@ -41,31 +96,13 @@ After running this command a new, updatated products file is created and be impo
 
 Example ran from the flowerfarm project root:
 
- $ obff forecast --forecast wrk/forecast.json --products wrk/products.json
+ $ obffctl forecast --forecast wrk/forecast.json --products wrk/products.json
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("forecast called")
-		forecastFile, _ := cmd.Flags().GetString("forecast")
-		fmt.Printf("forcastFile: %s\n", forecastFile)
 
-		// Open our jsonFile
-		jsonFile, err := os.Open(forecastFile)
-		// if we os.Open returns an error then handle it
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Printf("Successfully Opened %s", forecastFile)
-		// defer the closing of our jsonFile so that we can parse it later on
-		defer jsonFile.Close()
-
-		json_data, _ := ioutil.ReadAll(jsonFile)
-		forecast := new(Forecast)
-    err = json.Unmarshal(json_data, &forecast)
-
-		fmt.Printf("debub: \n%v", forecast)
-
-
+		forecast := loadForecast(cmd, args)
+		products := loadProducts(cmd, args)
+		updateProductsWithForecast(forecast, products)
 	},
 }
 
@@ -89,6 +126,78 @@ func init() {
 	forecastCmd.MarkFlagRequired("products")
 }
 
-// func updateProductsFromForecast(){
+func loadForecast(cmd *cobra.Command, args []string) *Forecast {
+	//
+	// Open forecastFile
+	//
+	forecastFileName, _ := cmd.Flags().GetString("forecast")
+	fmt.Printf("forcastFilePath: %s\n", forecastFileName)
+	forecastFile, err := os.Open(forecastFileName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-// }
+	fmt.Printf("Successfully Opened %s", forecastFileName)
+	// defer the closing of our forecastFile so that we can parse it later on
+	defer forecastFile.Close()
+
+	//
+	// Read file into forecast type
+	//
+	forecastJsonData, _ := ioutil.ReadAll(forecastFile)
+	forecast := new(Forecast)
+	err = json.Unmarshal(forecastJsonData, &forecast)
+
+	return forecast
+}
+
+func loadProducts(cmd *cobra.Command, args []string) *Products {
+	//
+	// Open productsFile
+	//
+	productsFileName, _ := cmd.Flags().GetString("products")
+	fmt.Printf("productsFilePath: %s\n", productsFileName)
+	productsFile, err := os.Open(productsFileName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Successfully Opened %s", productsFileName)
+	// defer the closing of our forecastFile so that we can parse it later on
+	defer productsFile.Close()
+
+	//
+	// Read file into products type
+	//
+	productsJsonData, _ := ioutil.ReadAll(productsFile)
+	products := new(Products)
+	err = json.Unmarshal(productsJsonData, &products)
+
+	return products
+}
+
+func updateProductsWithForecast(forecast *Forecast, products *Products) {
+
+	for _, productDoc := range *products {
+
+		fmt.Printf("Processing product sku: %s\n", productDoc.SKU)
+		findForecastDocBySKU(productDoc.SKU, forecast)
+		fmt.Println("------------------------------------------------------------------------------")
+	}
+
+}
+
+func findForecastDocBySKU(sku string, forecast *Forecast) ForecastDoc {
+
+	for _, forecastDoc := range *forecast {
+		if forecastDoc.SKU == sku {
+			fmt.Printf("\nFound forcast doc sku: %s\n", sku)
+			return forecastDoc
+		}
+	}
+
+	var errorDoc ForecastDoc
+	return errorDoc
+}
